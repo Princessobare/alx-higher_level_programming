7d2ecf69c24594dc11a5c6696ddd7e2fc1b09862
Obare <obaredafiaghor@gmail.com>
2023-07-17 04:55:17 -0700

    print integer list and retrieve element from list

---
+++ 0x03-python-data_structures/linked_lists.py
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - to print elements in a listint_t list
+ * @h: head of list pointer
+ * Return: returns the number of nodes in a list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+    const listint_t *current;
+    unsigned int n; /* number of nodes */
+
+    current = h;
+    n = 0;
+    while (current != NULL)
+    {
+	printf("%i\n", current->n);
+	current = current->next;
+	n++;
+    }
+
+    return (n);
+}
+
+/**
+ * add_nodeint_end - adds node to a listint_t list
+ * @head: listint_t list first node's pointer's pointer
+ * @n: integer for new node
+ * Return: on success return the new element address or NULL on failure
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+    listint_t *new;
+    listint_t *current;
+
+    current = *head;
+
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+	return (NULL);
+
+    new->n = n;
+    new->next = NULL;
+
+    if (*head == NULL)
+	*head = new;
+    else
+    {
+	while (current->next != NULL)
+	    current = current->next;
+	current->next = new;
+    }
+
+    return (new);
+}
+
+/**
+ * free_listint - clears listint_t list
+ * @head: pointer for list to be cleared
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+    listint_t *current;
+
+    while (head != NULL)
+    {
+	current = head;
+	head = head->next;
+	free(current);
+    }
+}
