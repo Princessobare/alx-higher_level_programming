27c761d70a6a858fd12762ea73e690f4013dde8d
Obare <obaredafiaghor@gmail.com>
2023-07-26 03:45:01 -0700

    python loops and functions

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# 12-fizzbuzz.py
+
+def fizzbuzz():
+    """Print from 1 to 100 demarcated by space.
+
+    For multiples of three, print Fizz.
+    For multiples of five, print Buzz.
+    For multiples of three and five, print FizzBuzz.
+    """
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(number), end="")
