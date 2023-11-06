f9437fd31ce81fbf5970bc6376ff2f1aedcb43c8
Obare <obaredafiaghor@gmail.com>
2023-11-06 08:16:52 -0800

    python network task contd

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This script:
+- takes a URL
+- sends POST request to the passed URL
+- takes email as parameter
+- displays body of response
+"""
+import sys
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
